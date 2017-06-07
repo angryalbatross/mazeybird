411e0091b6833f965d6fd0fc62174fcba5300b5b
Dan Burgett <daniel.burgett@gmail.com>
2017-06-06 21:52:00 -0500

    Created some new game objects and prefabs

---
+++ Assets/Scripts/rotateObject.js
@@ -0,0 +1,5 @@
+﻿public var degreesPerSec = 15;
+
+function Update () {
+    transform.Rotate (0,0,degreesPerSec*Time.deltaTime); //rotates 50 degrees per second around z axis
+}
---
+++ Assets/spawnGameObjectAtRandomLocation.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+public var gameObjectToSpawn : GameObject;
+public var arrayOfPoints : GameObject[];
+
+function Start () {
+	var spawnPointLocation = arrayOfPoints[Random.Range(0,arrayOfPoints.length - 1)].transform.position;
+	Instantiate(gameObjectToSpawn, spawnPointLocation, Quaternion.identity);
+}
