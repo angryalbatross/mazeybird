4c3e8d1afaf0ce975912939fa825e10768ab02df
Dan B <daniel.burgett@gmail.com>
2017-08-27 22:46:33 -0500

    Adding new framework for advertising

---
+++ Assets/Scripts/ShowAdOnClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowAdOnClick : MonoBehaviour {
+
+	public GameObject AdManager;
+	public GameObject MainMenu;
+
+	public void ShowAdToUser()
+	{
+#if UNITY_EDITOR
+		var currentMaxLevel = PlayerPrefs.GetInt("maxLevelUnlocked");
+		var currentMoney = PlayerPrefs.GetInt("gold");
+		PlayerPrefs.SetInt("gold", currentMoney + 10 + (2 * currentMaxLevel));
+		MainMenu.SetActive(true);
+#else
+		AdManager.SendMessage("ShowRewardedAd", SendMessageOptions.DontRequireReceiver);
+#endif
+	}
+}
