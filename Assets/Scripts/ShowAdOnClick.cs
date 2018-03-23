24a57f0577cf85f0f384edd47afbd2bb172d2773
Dan B <daniel.burgett@gmail.com>
2018-03-23 14:56:26 -0500

    committing laptop progress

--- Assets/Scripts/ShowAdOnClick.cs
+++ Assets/Scripts/ShowAdOnClick.cs
@@ -12,20 +12,20 @@ public class ShowAdOnClick : MonoBehaviour {
 	public void ShowAdToUser()
 	{
 		string lastAdWatchedDate = PlayerPrefs.GetString("lastAdDate");
-		if(lastAdWatchedDate == "" || DateTime.UtcNow > Convert.ToDateTime(lastAdWatchedDate).AddHours(22)) {
+		if(lastAdWatchedDate == "" || DateTime.UtcNow >= Convert.ToDateTime(lastAdWatchedDate).AddHours(22)) {
 			//then it has been 24 hours since we have watched an Ad
-		// #if UNITY_EDITOR
-		// 	var currentMaxLevel = PlayerPrefs.GetInt("maxLevelUnlocked");
-		// 	var currentMoney = PlayerPrefs.GetInt("gold");
-		// 	var currentLives = PlayerPrefs.GetInt("lives");
-		// 	PlayerPrefs.SetInt("gold", currentMoney + 10 + (2 * currentMaxLevel));
-		// 	PlayerPrefs.SetInt("lives", currentLives + 1);
-		// 	// set the last time that we watched an Ad
-		// 	PlayerPrefs.SetString("lastAdDate", DateTime.UtcNow.ToString());
-		// 	MainMenu.SetActive(true);
-		// #else
+		#if UNITY_EDITOR
+			var currentMaxLevel = PlayerPrefs.GetInt("maxLevelUnlocked");
+			var currentMoney = PlayerPrefs.GetInt("gold");
+			var currentLives = PlayerPrefs.GetInt("lives");
+			PlayerPrefs.SetInt("gold", currentMoney + 10 + (2 * currentMaxLevel));
+			PlayerPrefs.SetInt("lives", currentLives + 1);
+			// set the last time that we watched an Ad
+			PlayerPrefs.SetString("lastAdDate", DateTime.UtcNow.ToString());
+			MainMenu.SetActive(true);
+		#else
 			AdManager.SendMessage("ShowRewardedAd", SendMessageOptions.DontRequireReceiver);
-		// #endif
+		#endif
 		} else {
 			//show an alert that the user has to wait 24 hours between watching Ad's
 			AlreadyWatchedAdAlert.SetActive(true);
