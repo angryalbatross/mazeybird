465026b5de1f649b3e538511fe7e6ff3547be9ac
Drew Smithies <dschevyvette@gmail.com>
2018-04-16 22:10:47 -0500

    WIP spawn random blocks from main camera

---
+++ Assets/Scripts/MainCamera.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public var randomBlocks : GameObject;
+
+
+function Start () {
+    var childrenBlocks: Component[]; 
+    childrenBlocks = randomBlocks.GetComponentsInChildren(Transform, true);
+    for (var thisChild : Transform in childrenBlocks) {
+        if(thisChild.gameObject.name.IndexOf("RandomBlock") >= 0) {
+            Debug.Log(thisChild);
+        }
+    }
+}
+
+function Update () {
+	
+}
+
+function OnCollisionEnter2D(coll : Collision2D) {
+    Debug.Log(coll.gameObject.name);
+
+
+
+    // var thisBlock = [Random.Range(0,arrayOfBlocks.length - 1)];
+    // Instantiate(thisBlock, coll.gameObject.transform);
+}
+    
