54ea2891cc12bee5a02e780899f215ad4e83ce7b
Drew Smithies <dschevyvette@gmail.com>
2018-04-19 18:00:28 -0500

    WIP dont activate map connectors in between blocks

--- Assets/Scripts/MainCamera.js
+++ Assets/Scripts/MainCamera.js
@@ -22,22 +22,52 @@ function Update () {
 function OnTriggerEnter2D(coll : Collider2D) {
     if (coll.gameObject.tag == 'MapConnector') {
         Debug.Log(coll.gameObject.name);
-        getNewBlockOffset(coll.gameObject.name);
-
+        Debug.Log(getNewBlockOffset(coll.gameObject.name));
+        
         var blockToAdd : GameObject = randomBlocks[Random.Range(0,randomBlocks.length - 1)];
-        var newBlock = Instantiate(blockToAdd, coll.gameObject.transform.position, coll.gameObject.transform.rotation);
+        var blockToAddOffset : Hashtable = getNewBlockOffset(coll.gameObject.name);
+        var blockToAddChildren : Component[] = blockToAdd.GetComponentsInChildren(Transform, true);
+
+        // Destroy map connectors that are inbetween the existing block, and the new block
+        for (var thisChild : Transform in blockToAddChildren) {
+            if(thisChild.gameObject.name.IndexOf('_' + blockToAddOffset['opposite'] + '_') > 0) {
+                Destroy(thisChild.gameObject);
+            }
+        }
+
+        var newBlock = Instantiate(blockToAdd, blockToAddOffset['blockPosition'], coll.gameObject.transform.rotation);
         newBlock.SetActive(true);
         Destroy(coll.gameObject);
     }
 }
 
-function getNewBlockOffset(connectorName : String) {
+function getNewBlockOffset(connectorName : String) : Hashtable {
+    var result : Hashtable;
     var nameArray : String[];
     nameArray = connectorName.Split("_"[0]);
     Debug.Log(nameArray[0]);
 
+    var blockPosition : Vector3;
     switch (nameArray[1]) {
+        case 'bot':
+            blockPosition = Vector3(0.0, -20.0);
+            result.Add('opposite', 'top');
+            break;
+        case 'top':
+            blockPosition = Vector3(0.0, 20.0);
+            result.Add('opposite', 'bot');
+            break;
+        case 'left':
+            blockPosition = Vector3(-20.0, 0.0);
+            result.Add('opposite', 'right');
+            break;
+        case 'right':
+            blockPosition = Vector3(20.0, 0.0);
+            result.Add('opposite', 'left');
+            break;
     }
+    result.Add('blockPosition', blockPosition);
+    return result;
 }
 
     // var thisBlock = [Random.Range(0,arrayOfBlocks.length - 1)];
