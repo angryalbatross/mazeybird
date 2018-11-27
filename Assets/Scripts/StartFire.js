6755bcc557796e8dbaca147b1288be09a28e0ca2
Dan Burgett <daniel.burgett@gmail.com>
2018-11-27 16:24:14 -0600

    Updating with start fire code

---
+++ Assets/Scripts/StartFire.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+public var fireEffect : ParticleSystem;
+public var player : GameObject;
+private var touchingBlock : boolean = false;
+
+function FixedUpdate ()
+{
+  if(touchingBlock){
+    Debug.Log("SOMETHING TOUCHED ME");
+  }
+ }
+
+function OnTriggerEnter(other : Collider) {
+     if (other.tag == "Player") {
+         touchingBlock = true;
+     }
+ }
+
+function OnTriggerExit(other : Collider) {
+     if (other.tag == "Player") {
+         touchingBlock = false;
+     }
+ }
